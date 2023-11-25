619efc57b0650ea7073f149ea6d9ae892d51ef9f
Joy S <stepjoyous@gmail.com>
2023-11-25 15:14:30 -0800

    Created pseudocode notes for routes planning.

--- server.js
+++ server.js
@@ -4,6 +4,20 @@ const PORT = 3030
 
 const app = express()
 
+// Routes
+app.get('/', (req, res) => {
+  res.send('This is working')
+})
+
 app.listen(PORT, () => {
   console.log(`App Server is listening on PORT:${PORT}`)
 })
+
+/*
+// General Route Planning
+  / --> res = this is working
+  /signin --> POST = success/fail
+  /register --> POST = user
+  /profile/:userId --> GET = user
+  /image --> PUT --> user
+*/
