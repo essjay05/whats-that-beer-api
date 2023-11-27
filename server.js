5969637cc59f68a31b2ca953e0fe57f3ea59b309
Joy S <stepjoyous@gmail.com>
2023-11-26 23:12:10 -0800

    Implementing bcrypt-nodejs for register and signin routes. Still WIP.

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require('express')
-
+const bcrypt = require('bcrypt-nodejs')
 const PORT = 3030
 
 const app = express()
@@ -26,6 +26,13 @@ const database = {
       entries: 0,
       joined: new Date()
     },
+  ],
+  login: [
+    {
+      id: '987',
+      hash: '',
+      email: 'john@gmail.com'
+    }
   ]
 }
 
@@ -37,6 +44,15 @@ app.get('/', (req, res) => {
 
 // Signin
 app.post('/signin', (req, res) => {
+  // Load hash from your password DB.
+  bcrypt.compare("apples", "$2a$10$c4iMGUnqh3y0.EIRPWXePOr3SW7i6HXG4OZ6BI5U1fRwjl4/XV8gG", function(err, res) {
+    // res == true
+    console.log('First guess', res)
+  });
+  bcrypt.compare("seeya", "$2a$10$c4iMGUnqh3y0.EIRPWXePOr3SW7i6HXG4OZ6BI5U1fRwjl4/XV8gG", function(err, res) {
+    // res = false
+    console.log('Second guess', res)
+});
   if(req.body.email === database.users[0].email &&
     req.body.password === database.users[0].password) {
       res.status(200).json('Successfully signed in')
@@ -48,6 +64,10 @@ app.post('/signin', (req, res) => {
 // Register
 app.post('/register', (req, res) => {
   const { username, email, password } = req.body
+  bcrypt.hash(password, null, null, function(err, hash) {
+    // Store hash in your password DB.
+    console.log(hash)
+  });
   database.users.push({
     id: '125',
     username: username,
@@ -90,6 +110,19 @@ app.put('/image', (req, res) => {
     res.status(400).json('User not found')
   }
 })
+
+// Hashing passwords logic
+bcrypt.hash("aloha", null, null, function(err, hash) {
+  // Store hash in your password DB.
+});
+
+// // Load hash from your password DB.
+// bcrypt.compare("aloha", hash, function(err, res) {
+//     // res == true
+// });
+// bcrypt.compare("seeya", hash, function(err, res) {
+//     // res = false
+// });
 
 app.listen(PORT, () => {
   console.log(`App Server is listening on PORT:${PORT}`)
