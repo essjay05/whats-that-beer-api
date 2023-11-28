0b0640040db88da664fdfb41913da158745cc7ec
Joy S <stepjoyous@gmail.com>
2023-11-27 21:28:43 -0800

    Created Beer mongoose model to be used.

---
+++ models/Beer.js
@@ -0,0 +1,18 @@
+const
+  mongoose = require('mongoose'),
+  beerSchema = new mongoose.Schema({
+    beerId: { type: String, required: true },
+    name: { type: String },
+    abv: { type: String },
+    brewery: { type: String},
+    description: { type: String},
+    photos: [{
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Photo' /* Photo Schema *Bonus* */}],
+    users: [{
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User' }]
+  }, { timestamps: true })
+
+  const Beer = mongoose.model('Beer', beerSchema)
+  module.exports = Beer
