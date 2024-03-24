ee772d9df35a59e158b3567506d0c5f5c8fcf0c5
Joy S <stepjoyous@gmail.com>
2024-03-24 11:19:11 -0700

    Added global error handling and appError utils file.

--- server.js
+++ server.js
@@ -29,6 +29,17 @@ app.use(express.static(path.join(__dirname, 'client', 'build')))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 
+// Set Global Error Handler
+app.use((err, req, res, next) => {
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || 'error';
+
+  res.status(err.statusCode).json({
+    status: err.status,
+    message: err.message,
+  })
+})
+
 // Set API specific root
 app.get('/api', (req, res) => {
   res.json({ message: 'API ROOT' })
