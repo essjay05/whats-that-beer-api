af1ea626d449fbc33cb317ca579eb91a50333f06
Joy S <stepjoyous@gmail.com>
2024-03-25 22:09:04 -0700

    Factored out assigning JWT to user function to AuthUtils.js

--- utils/AuthUtils.js
+++ utils/AuthUtils.js
@@ -1,4 +1,9 @@
+require('dotenv').config()
+
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken')
+
+const jwtKey = process.env.JWT_KEY
 
 const hash = async (text, size) => {
   try {
@@ -10,6 +15,15 @@ const hash = async (text, size) => {
   }
 }
 
+const tokenizeUser = async (userId) => {
+  // Assign JWT to user
+  const userToken = jwt.sign({ _id: userId}, jwtKey, {
+    expiresIn: '1d',
+  })
+  return userToken
+}
+
 module.exports = {
-  hash
+  hash,
+  tokenizeUser
 }
