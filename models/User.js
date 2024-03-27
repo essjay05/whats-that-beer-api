3bf950929a404e9ef279d4bef8872b7d51d6f086
Joy S <stepjoyous@gmail.com>
2024-03-26 21:43:51 -0700

    Updating User model.

--- models/User.js
+++ models/User.js
@@ -1,7 +1,6 @@
-const
-  mongoose = require('mongoose'),
+const mongoose = require('mongoose')
   // bcrypt = require('bcrypt-nodejs'),
-  userSchema = new mongoose.Schema({
+const userSchema = new mongoose.Schema({
     name: { type: String, required: true },
     email: { type: String, required: true, unique: true },
     role: { type: String, default: "user" },
