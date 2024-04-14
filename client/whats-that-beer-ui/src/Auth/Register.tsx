01ef457506184692862984981dce93ee646692e2
Joy S <stepjoyous@gmail.com>
2024-04-14 13:16:09 -0700

    Added register image.

--- client/whats-that-beer-ui/src/Auth/Register.tsx
+++ client/whats-that-beer-ui/src/Auth/Register.tsx
@@ -1,8 +1,13 @@
 import { Button, Card, Flex, Form, Input, Typography, type FormProps } from "antd";
+import { Link } from "react-router-dom"
+import signupImage from '../../public/undraw_sign_up_n6im(1).svg'
+import './Register.css'
 
 type FieldType = {
   name?: string;
+  email?: string;
   password?: string;
+  passwordConfirm?: string;
   remember?: string;
 };
 
@@ -18,7 +23,7 @@ const Register = () => {
 
   return (
     <Card className="form-container">
-      <Flex>
+      <Flex gap="large" align="center">
         <Flex vertical flex={1}>
           <Typography.Title
             level={3}
@@ -44,19 +49,48 @@ const Register = () => {
               <Input placeholder="Enter your full name"/>
             </Form.Item>
             <Form.Item<FieldType>
+              label="Email"
+              name="email"
+              rules={[
+                { required: true, message: "Please input your email."},
+                { type: 'email', message: 'The input is not a valid email.'}
+              ]}>
+              <Input placeholder="Enter your email"/>
+            </Form.Item>
+            <Form.Item<FieldType>
               label="Password"
               name="password"
               rules={[{ required: true, message: 'Please input your password.' }]}>
               <Input.Password placeholder="Enter your password" />
             </Form.Item>
-            <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
-              <Button type="primary" htmlType="submit">
-                Submit
+            <Form.Item<FieldType>
+              label="Confirm Password"
+              name="passwordConfirm"
+              rules={[{ required: true, message: 'Please confirm your password.' }]}>
+              <Input.Password placeholder="Re-enter your password" />
+            </Form.Item>
+            <Form.Item>
+              <Button
+                type="primary"
+                htmlType="submit"
+                className="btn">
+                Create Account
               </Button>
+            </Form.Item>
+            <Form.Item>
+              <Link
+                to="/login">
+                <Button className="btn">Sign In</Button>
+              </Link>
             </Form.Item>
           </Form>
         </Flex>
-        {/* <Flex></Flex> */}
+        <Flex flex={1}>
+          <img
+            className="register-img"
+            alt="Illustration of online form with a person standing next to it."
+            src={signupImage}/>
+        </Flex>
       </Flex>
     </Card>
   )
