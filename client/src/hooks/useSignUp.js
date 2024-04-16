cf1d4aff887762b06d6f12eccbdfeacede1f37f0
Joy S <stepjoyous@gmail.com>
2024-04-15 21:26:18 -0700

    Completed useSignUp logic WIP: testing.

--- client/src/hooks/useSignUp.js
+++ client/src/hooks/useSignUp.js
@@ -1,7 +1,41 @@
+import { useState } from 'react'
+import { message } from 'antd'
+import { useAuth } from '../contexts/AuthContext.jsx'
+
 const useSignUp = () => {
-  return (
-    <div>useSignUp</div>
-  )
+  const login = useAuth()
+  const [error, setError] = useState(null)
+  const [loading, setLoading] = useState(null )
+
+  const registerUser = async (values) => {
+    if (values.password !== values.passwordConfirm) {
+      return setError('Passwords do not match!')
+    }
+
+    try {
+      setError(null)
+      setLoading(false)
+      const res = await fetch(`http://localhost:${import.meta.env.PORT}/api/signup`, {
+        method: 'POST',
+        body: JSON.stringify(values)
+      })
+
+      const data = await res.json()
+      if (res.status === 201) {
+        message.success(data.message)
+      } else if (res.status === 4000) {
+        setError(data.message)
+      } else {
+        message.error('Registration failed')
+      }
+    } catch(err) {
+      message.error(err)
+    } finally {
+      setLoading(false)
+    }
+  }
+
+  return { loading, error, registerUser }
 }
 
 export default useSignUp
