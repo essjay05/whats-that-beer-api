cfff6172d685dc68df66d4764f6d4cc79380c543
Joy S <stepjoyous@gmail.com>
2024-04-18 22:45:45 -0700

    Fixed CORS issue and created a new user successfully but the values were null. WIP: saving form value data and fixing console.log React object error.

--- client/src/hooks/useSignUp.js
+++ client/src/hooks/useSignUp.js
@@ -3,28 +3,41 @@ import { message } from 'antd'
 import { useAuth } from '../contexts/AuthContext.jsx'
 
 const useSignUp = () => {
-  const login = useAuth()
+  const { login } = useAuth()
   const [error, setError] = useState(null)
   const [loading, setLoading] = useState(null )
+
+  const mongoDbRegisterEndpoint = `${import.meta.env.VITE_USER_API_URL}`
 
   const registerUser = async (values) => {
     if (values.password !== values.passwordConfirm) {
       return setError('Passwords do not match!')
     }
 
+    const signupBody = JSON.stringify(values)
+    console.log(`signupBody:`)
+    console.log(signupBody)
+
     try {
       setError(null)
       setLoading(false)
-      const res = await fetch(`http://localhost:${import.meta.env.PORT}/api/signup`, {
+      const res = await fetch(mongoDbRegisterEndpoint, {
+        mode: 'no-cors',
         method: 'POST',
-        body: JSON.stringify(values)
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: signupBody
       })
 
       const data = await res.json()
+      console.log(`data`)
+      console.log(data)
       if (res.status === 201) {
         message.success(data.message)
       } else if (res.status === 4000) {
         setError(data.message)
+        login(data.token, data.user)
       } else {
         message.error('Registration failed')
       }
