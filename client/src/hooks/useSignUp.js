639a0351eb8d6b8d5bcb022e664fe40e0fbbb957
Joy S <stepjoyous@gmail.com>
2024-04-20 20:02:42 -0700

    WIP: registering user via frontend register form.

--- client/src/hooks/useSignUp.js
+++ client/src/hooks/useSignUp.js
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import { message } from 'antd'
+import axios from 'axios'
 import { useAuth } from '../contexts/AuthContext.jsx'
 
 const useSignUp = () => {
@@ -14,35 +15,55 @@ const useSignUp = () => {
       return setError('Passwords do not match!')
     }
 
+    console.log(`registerUser values:`)
+    console.log(values)
+
     const signupBody = JSON.stringify(values)
-    console.log(`signupBody:`)
+    console.log(`signupBody:`) 
     console.log(signupBody)
+
+    const { name, email, password } = values
+
+    console.log(`name: ${name}, email: ${email}, password: ${password}`)
+
+    axios.defaults.headers.post['Content-Type'] ='application/json; charset=utf-8';
+    axios.defaults.headers.post['Access-Control-Allow-Origin'] = '*';
 
     try {
       setError(null)
       setLoading(false)
-      const res = await fetch(mongoDbRegisterEndpoint, {
-        mode: 'no-cors',
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json'
+      // const res = await fetch(mongoDbRegisterEndpoint, {
+      //   mode: 'no-cors',
+      //   method: 'POST',
+      //   headers: {
+      //     'Content-Type': 'application/json',
+      //   },
+      //   body: signupBody
+      // })
+
+      const res = await axios.post(
+        mongoDbRegisterEndpoint,
+        {
+          name: name,
+          email: email,
+          password: password
         },
-        body: signupBody
-      })
+        {headers})
 
       const data = await res.json()
       console.log(`data`)
       console.log(data)
+
       if (res.status === 201) {
-        message.success(data.message)
-      } else if (res.status === 4000) {
+        message.success('User successfully registered!')
+      } else if (res.status === 400) {
         setError(data.message)
         login(data.token, data.user)
       } else {
-        message.error('Registration failed')
+        message.error('Registration failed.')
       }
     } catch(err) {
-      message.error(err)
+      message.error('Registration failed.')
     } finally {
       setLoading(false)
     }
