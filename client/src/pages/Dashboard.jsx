c49a5d75c425002abd25f19947db40813a0e3810
Joy S <stepjoyous@gmail.com>
2024-04-25 20:13:55 -0700

    Add basic functionality to Dashboard to logout if/when logged in.

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,6 +1,15 @@
+import { Button } from 'antd'
+import { useAuth } from '../contexts/AuthContext.jsx'
+
 const Dashboard = () => {
+  
+  const { logout } = useAuth()
+
   return (
-    <div>Dashboard</div>
+    <>
+      <div>Dashboard</div>
+      <Button onClick={logout}>Logout</Button>
+    </>
   )
 }
 
