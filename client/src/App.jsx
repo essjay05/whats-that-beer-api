55c28b9d0e81051100525553089e1230c4ef890d
Joy S <stepjoyous@gmail.com>
2024-04-25 20:16:43 -0700

    Updated App.js routes to check whether the user is authenticated and reroute them accordingly based on that condition.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,16 +1,26 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom'
 import './App.css'
 
 import Register from './Auth/Register'
 import Login from './Auth/Login'
 import Dashboard from './pages/Dashboard'
+import { useAuth } from './contexts/AuthContext'
 
 const App = () => { 
+
+  const { isAuthenticated } = useAuth()
+
   return <Router>
     <Routes>
-      <Route path='/' element={<Register/>}></Route>
-      <Route path='/login' element={<Login/>}></Route>
-      <Route path='/dashboard' element={<Dashboard/>}></Route>
+      <Route path='/' element={ 
+        !isAuthenticated ? <Register/> : <Navigate to='/dashboard' />
+      }></Route>
+      <Route path='/login' element={
+        !isAuthenticated ? <Login/> : <Navigate to='/dashboard'/>
+      }></Route>
+      <Route path='/dashboard' element={
+        isAuthenticated ? <Dashboard/> : <Login />
+      }></Route>
     </Routes>
   </Router>
 }
