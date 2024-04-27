690326b20c30fb89551da293b199c17d5976b37f
Joy S <stepjoyous@gmail.com>
2024-04-27 07:48:56 -0700

    Updated Dashboard to show user info upon successful login.

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,15 +1,47 @@
-import { Button } from 'antd'
+import { Avatar, Button, Card, Flex, Typography } from 'antd'
+import { UserOutlined } from '@ant-design/icons'
 import { useAuth } from '../contexts/AuthContext.jsx'
+import { useEffect } from 'react'
 
 const Dashboard = () => {
   
-  const { logout } = useAuth()
+  const { userData, logout } = useAuth()
+
+  useEffect(() => {
+    console.log(`userData:`)
+    console.log(userData)
+  },[])
+
+  const handleLogout = async() => {
+    await logout()
+  }
 
   return (
-    <>
-      <div>Dashboard</div>
-      <Button onClick={logout}>Logout</Button>
-    </>
+    <Card className='profile-card'>
+      <Flex vertical gap='small' align='center'>
+        <Avatar
+          size={150}
+          icon={
+          <UserOutlined className='avatar '/>
+        }/>
+        <Typography.Title
+          level={2}
+          strong
+          className='username'>
+          {userData.name}
+        </Typography.Title>
+        <Typography.Text type='secondary' strong>
+          Email: {userData.email}
+        </Typography.Text>
+        <Button
+          className='profile-btn'
+          size='large'
+          type='primary'
+          onClick={handleLogout}>
+          Logout
+        </Button>
+      </Flex>
+    </Card>
   )
 }
 
