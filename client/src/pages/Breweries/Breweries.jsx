969ed7fde3cf02af804e39a06f81835f796abc1c
Joy S <stepjoyous@gmail.com>
2024-04-28 21:13:05 -0700

    Created Breweries base page and tested routing.

---
+++ client/src/pages/Breweries/Breweries.jsx
@@ -0,0 +1,12 @@
+import NavBar from "../../global/NavBar/NavBar"
+
+const Breweries = () => {
+  return (
+    <div className='page-container'>
+      <NavBar/>
+      <h1>Breweries</h1>
+    </div>
+  )
+}
+
+export default Breweries
