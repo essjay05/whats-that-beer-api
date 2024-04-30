9061ffd5672d0eb95f48e7cee2ae58d3fd647e48
Joy S <stepjoyous@gmail.com>
2024-04-29 21:42:10 -0700

    Added Navbar to Dashboard page.

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,6 +1,7 @@
 import { Avatar, Button, Card, Flex, Typography } from 'antd'
 import { UserOutlined } from '@ant-design/icons'
 import { useAuth } from '../contexts/AuthContext.jsx'
+import NavBar from '../global/NavBar/NavBar.jsx'
 
 import './Dashboard.css'
 
@@ -13,31 +14,34 @@ const Dashboard = () => {
   }
 
   return (
-    <Card className='profile-card'>
-      <Flex vertical gap='small' align='center'>
-        <Avatar
-          size={150}
-          icon={
-          <UserOutlined className='avatar '/>
-        }/>
-        <Typography.Title
-          level={2}
-          strong
-          className='username'>
-          {userData.name}
-        </Typography.Title>
-        <Typography.Text type='secondary' strong>
-          Email: {userData.email}
-        </Typography.Text>
-        <Button
-          className='profile-btn'
-          size='large'
-          type='primary'
-          onClick={handleLogout}>
-          Logout
-        </Button>
-      </Flex>
-    </Card>
+    <div className='page-container dashboard'>
+      <NavBar />
+      <Card className='profile-card'>
+        <Flex vertical gap='small' align='center'>
+          <Avatar
+            size={150}
+            icon={
+            <UserOutlined className='avatar '/>
+          }/>
+          <Typography.Title
+            level={2}
+            strong
+            className='username'>
+            {userData.name}
+          </Typography.Title>
+          <Typography.Text type='secondary' strong>
+            Email: {userData.email}
+          </Typography.Text>
+          <Button
+            className='profile-btn'
+            size='large'
+            type='primary'
+            onClick={handleLogout}>
+            Logout
+          </Button>
+        </Flex>
+      </Card>
+    </div>
   )
 }
 
