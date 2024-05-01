e8f28cd1212706ac2177ef996256f62d318f9256
Joy S <stepjoyous@gmail.com>
2024-04-30 17:24:10 -0700

    Created Beers page and added NavBar. Tested successfully.

---
+++ client/src/pages/Beers/Beers.jsx
@@ -0,0 +1,13 @@
+import './BeerStyles.css'
+import NavBar from '../../global/NavBar/NavBar.jsx'
+
+const Beers = () => {
+  return (
+    <div className='page-container'>
+      <NavBar/>
+      <h1>Beers</h1>
+    </div>
+  )
+}
+
+export default Beers
