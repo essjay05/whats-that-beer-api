488220757fba7149f296c0e63a2ec37221816a39
Joy S <stepjoyous@gmail.com>
2024-05-02 21:59:53 -0700

    Created placeholder for create brewery form.

---
+++ client/src/pages/Breweries/CreateBrewery.jsx
@@ -0,0 +1,18 @@
+// import { Alert, Button, Card, Flex, Form, Input, Spin, Typography } from 'antd';
+// import { Link } from 'react-router-dom'
+
+import NavBar from "../../global/NavBar/NavBar"
+
+const CreateBrewery = () => {
+  return (
+    <div className='page-container'>
+      <NavBar/>
+      <h1>Create Brewery</h1>
+      <section>
+        <h2>Add Brewery Form here:</h2>
+      </section>
+    </div>
+  )
+}
+
+export default CreateBrewery
