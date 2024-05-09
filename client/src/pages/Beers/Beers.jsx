d2b6b8799586750545bab272554ee4f23a74c39b
Joy S <stepjoyous@gmail.com>
2024-05-09 16:44:06 -0700

    Added links side nav to beers page.

--- client/src/pages/Beers/Beers.jsx
+++ client/src/pages/Beers/Beers.jsx
@@ -1,11 +1,18 @@
 import './BeerStyles.css'
 import NavBar from '../../global/NavBar/NavBar.jsx'
+import { Link } from 'react-router-dom'
 
 const Beers = () => {
   return (
     <div className='page-container'>
       <NavBar/>
       <h1>Beers</h1>
+      <aside className='side-nav'>
+        <ul>
+          <li><Link to='/create-beer'>Add a Beer</Link></li>
+          <li>Favorited Beers</li>
+        </ul>
+      </aside>
     </div>
   )
 }
