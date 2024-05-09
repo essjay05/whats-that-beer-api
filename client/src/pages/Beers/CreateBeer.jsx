a68fbbc716ca5ce18e1f57893dbea3b72067cd56
Joy S <stepjoyous@gmail.com>
2024-05-08 21:51:18 -0700

    Created CreateBeer page.

---
+++ client/src/pages/Beers/CreateBeer.jsx
@@ -0,0 +1,18 @@
+// import { Alert, Button, Card, Flex, Form, Input, Spin, Typography } from 'antd';
+// import { Link } from 'react-router-dom'
+
+import NavBar from "../../global/NavBar/NavBar"
+
+const CreateBeer = () => {
+  return (
+    <div className='page-container'>
+      <NavBar/>
+      <h1>Create Beer</h1>
+      <section>
+        <h2>Add Beer Form here:</h2>
+      </section>
+    </div>
+  )
+}
+
+export default CreateBeer
